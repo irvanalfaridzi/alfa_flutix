330eab41a9f3cb3e17fcd4ea382f88567751e233
Alfaridzi <irvanalfaridzi@gmail.com>
2020-09-25 10:22:53 +0700

    login page

--- lib/ui/pages/pages.dart
+++ lib/ui/pages/pages.dart
@@ -1,9 +1,12 @@
 import 'package:alfa_flutix/bloc/blocs.dart';
 import 'package:alfa_flutix/services/services.dart';
 import 'package:alfa_flutix/shared/shared.dart';
+import 'package:email_validator/email_validator.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flushbar/flushbar.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 part 'sign_in_page.dart';
--- lib/ui/pages/sign_in_page.dart
+++ lib/ui/pages/sign_in_page.dart
@@ -18,90 +18,141 @@ class _SignInPageState extends State<SignInPage> {
     context
         .bloc<ThemeBloc>()
         .add(ChangeTheme(ThemeData().copyWith(primaryColor: accentColor2)));
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Container(
-        padding: EdgeInsets.symmetric(horizontal: defaultMargin),
-        child: ListView(
-          children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                SizedBox(
-                  height: 30,
-                ),
-                SizedBox(height: 70, child: Image.asset("assets/logo.png")),
-                Container(
-                  margin: EdgeInsets.only(top: 70, bottom: 40),
-                  child: Text(
-                    "Welcome back,\nExplorer!",
-                    style: blackTextFont.copyWith(fontSize: 20),
+
+    return WillPopScope(
+      onWillPop: () {
+        context.bloc<PageBloc>().add(GoToSplashPage());
+        return;
+      },
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Container(
+          padding: EdgeInsets.symmetric(horizontal: defaultMargin),
+          child: ListView(
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  SizedBox(
+                    height: 30,
                   ),
-                ),
-                TextField(
-                  controller: emailController,
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10)),
-                      labelText: "Email Address",
-                      hintText: "Email Address"),
-                ),
-                SizedBox(
-                  height: 16,
-                ),
-                TextField(
-                  controller: passwordController,
-                  obscureText: true,
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10)),
-                      labelText: "Password",
-                      hintText: "Password"),
-                ),
-                SizedBox(
-                  height: 6,
-                ),
-                Row(
-                  children: <Widget>[
-                    Text(
-                      "Forgot Password? ",
-                      style: greyTextFont.copyWith(
-                          fontSize: 12, fontWeight: FontWeight.w400),
-                    ),
-                    Text(
-                      "Get Now",
-                      style: purpleTextFont.copyWith(fontSize: 12),
-                    )
-                  ],
-                ),
-                Center(
-                  child: Container(
-                    width: 50,
-                    height: 50,
-                    margin: EdgeInsets.only(top: 40, bottom: 30),
-                    child: FloatingActionButton(
-                      child: Icon(Icons.arrow_forward),
-                      backgroundColor: mainColor,
-                      onPressed: () {},
+                  SizedBox(height: 70, child: Image.asset("assets/logo.png")),
+                  Container(
+                    margin: EdgeInsets.only(top: 70, bottom: 40),
+                    child: Text(
+                      "Welcome back,\nExplorer!",
+                      style: blackTextFont.copyWith(fontSize: 20),
                     ),
                   ),
-                ),
-                Row(
-                  children: <Widget>[
-                    Text(
-                      "Start Fresh Now? ",
-                      style: greyTextFont.copyWith(fontWeight: FontWeight.w400),
+                  TextField(
+                    onChanged: (text) {
+                      setState(() {
+                        isEmaiValid = EmailValidator.validate(text);
+                      });
+                    },
+                    controller: emailController,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10)),
+                        labelText: "Email Address",
+                        hintText: "Email Address"),
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  TextField(
+                    onChanged: (text) {
+                      setState(() {
+                        isPasswordValid = text.length >= 6;
+                      });
+                    },
+                    controller: passwordController,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10)),
+                        labelText: "Password",
+                        hintText: "Password"),
+                  ),
+                  SizedBox(
+                    height: 6,
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Text(
+                        "Forgot Password? ",
+                        style: greyTextFont.copyWith(
+                            fontSize: 12, fontWeight: FontWeight.w400),
+                      ),
+                      Text(
+                        "Get Now",
+                        style: purpleTextFont.copyWith(fontSize: 12),
+                      )
+                    ],
+                  ),
+                  Center(
+                    child: Container(
+                      width: 50,
+                      height: 50,
+                      margin: EdgeInsets.only(top: 40, bottom: 30),
+                      child: isSigningIn
+                          ? SpinKitFadingCircle(
+                              color: mainColor,
+                            )
+                          : FloatingActionButton(
+                              elevation: 0,
+                              child: Icon(Icons.arrow_forward,
+                                  color: isEmaiValid && isPasswordValid
+                                      ? Colors.white
+                                      : Color(0xFFBEBEBE)),
+                              backgroundColor: isEmaiValid && isPasswordValid
+                                  ? mainColor
+                                  : Color(0xFFE4E4E4),
+                              onPressed: isEmaiValid && isPasswordValid
+                                  ? () async {
+                                      setState(() {
+                                        isSigningIn = true;
+                                      });
+
+                                      SignInSignUpResult result =
+                                          await AuthServices.signIn(
+                                              emailController.text,
+                                              passwordController.text);
+
+                                      if (result.user == null) {
+                                        setState(() {
+                                          isSigningIn = false;
+                                        });
+                                        Flushbar(
+                                          duration: Duration(seconds: 4),
+                                          flushbarPosition:
+                                              FlushbarPosition.TOP,
+                                          backgroundColor: Color(0xFFFF5C83),
+                                          message: result.message,
+                                        )..show(context);
+                                      }
+                                    }
+                                  : null),
                     ),
-                    Text(
-                      "Sign Up",
-                      style: purpleTextFont,
-                    )
-                  ],
-                )
-              ],
-            ),
-          ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Text(
+                        "Start Fresh Now? ",
+                        style:
+                            greyTextFont.copyWith(fontWeight: FontWeight.w400),
+                      ),
+                      Text(
+                        "Sign Up",
+                        style: purpleTextFont,
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
